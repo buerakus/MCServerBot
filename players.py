fb04dc60adaa363f985e705e6dad17ccc300b0a7
buerakus <112691067+buerakus@users.noreply.github.com>
2023-02-09 14:41:51 +0800

    Delete players.py

--- players.py
+++
@@ -1,6 +0,0 @@
-from mcstatus import JavaServer
-server = JavaServer.lookup("95.216.62.170:25842")
-status = server.status()
-query = server.query()
-listss = {', '.join(query.players.names)}
-print(listss)
